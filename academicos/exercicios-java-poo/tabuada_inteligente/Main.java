f48ba6a37713aacc52c5c45bd9d869b67b602523
Giovanna Nascimento Chaves <giovannanaschaves@gmail.com>
2026-04-02 16:36:32 -0300

    Implement intelligent multiplication table in Main.java

---
+++ academicos/exercicios-java-poo/tabuada_inteligente/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero <= 0) {
+            System.out.println("Número inválido. Por favor, digite um número maior que zero.");
+            return;
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            if(resultado % 2 == 0) {
+                System.out.println(numero + "x" + i + "=" + resultado + "(Par)");
+            } else {
+                System.out.println(numero + "x" + i + "=" + resultado + "(Ímpar)");   
+            }    
+            }
+        }
+    }
